e7950183547321649dfaeaba621dc85ba32c4530
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-28 08:54:22 +0530

    Create challenge(1).java
    Program of adding two number taking input from the user in java.

---
+++ challenge(1).java
@@ -0,0 +1,20 @@
+
+// Program for Adding Two Numbers taking Input From the user;
+
+
+import java.util.Scanner;
+
+public class learningScan{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Value of A");
+        int a = input.nextInt();
+        System.out.println("Enter The Value of B");
+        int b = input.nextInt();
+      
+      System.out.println("The value of a =" + " " + a + " "+ "and value of b = "+ " " + b + " So, (A+B) = " + " " + (a+b));         
+    }
+}
+
+
+
