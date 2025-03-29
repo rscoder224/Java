2b68a4dbbbe1b7aa7679e3458bbf945a3e3083d7
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 12:52:36 +0530

    Create unaryOperator.java
    Using Using operators in Java.

---
+++ unaryOperator.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+public class unaryOpretors{
+    public static void main(String[] args){
+    int a = 10;
+    int b = 3;
+    
+    int c = -a ;
+    System.out.println(c);
+    // Pre and Post increment Operators
+    int d = ++a;
+    System.out.println(d);
+    int e = a++;
+    System.out.println(e);
+    
+         // Pre and Post Decrement Operators
+    
+    int f = a--;
+    System.out.println(f);
+    
+    int g = --a;
+    System.out.println(g);
+    
+    }
+    
+}
