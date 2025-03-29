03ab0919bef707750cc525d37ce76510260842b6
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 11:37:01 +0530

    Create swapChallenge.java
    Number Swap Challenge done in java

---
+++ swapChallenge.java
@@ -0,0 +1,20 @@
+// Swap program Challenge 
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class swapChallenge{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number a");
+        int num1 = input.nextInt();
+         
+        System.out.println("Enter a number b");
+        int num2 = input.nextInt();
+        
+        int a = num2;
+        System.out.println("Value of a" + " " +a);
+        int b = num1;
+        System.out.println("Value of b" + " " + b);
+    }
+}
