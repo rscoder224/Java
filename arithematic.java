8d6962c735c399482b1fb0604b0e285c5618e042
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 11:51:04 +0530

    Create arithematic.java
    arithematic operators

---
+++ arithematic.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+
+// Arithematic Oprators In java
+// + - * / %;
+
+public class arithematic{
+    public static void main(String[] args){
+        int a = 35;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        
+        double x = 1.89;
+        double y = 2.76;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
