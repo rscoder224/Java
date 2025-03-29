0f391b31e69965d68479d33fd8eb8d536d0ecf19
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 11:07:11 +0530

    Create Assignment_in.java
    Create Assignment_in.java

---
+++ Assignment_in.java
@@ -0,0 +1,13 @@
+// assignment Operator in Java
+
+import java.lang.*;
+
+public class assignment{
+    public static void main(String[] args){
+        int myInt = 9;
+        int newInt = myInt;
+        System.out.println(newInt);
+    
+        System.out.println(myInt);
+    }
+}
