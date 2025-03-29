9c052aa7987386a9c8284d0c07e3fa6670cc13f6
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 13:28:44 +0530

    Create challenge(11).java
    Create a program to calculate Area of triangle

---
+++ challenge(11).java
@@ -0,0 +1,22 @@
+
+// Challenge
+// Create a program to calculate Area of triangle
+
+
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class challenge_10{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Base =" + " ");
+        double a = input.nextDouble();
+        System.out.print("Enter height =" +" ");
+        double b = input.nextDouble();
+        
+        
+System.out.println("The area of triagle is  =" +  " "+ (0.5*a*b));
+       
+    }
+}
