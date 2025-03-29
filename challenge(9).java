0a206fda754513c65409a7ac1a3b629c5a3d8f2d
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 13:14:19 +0530

    Create challenge(9).java
     Create a program to calculate product of two floating point numbers

---
+++ challenge(9).java
@@ -0,0 +1,22 @@
+
+// Challenge
+// Create a program to calculate product of two floating point numbers
+
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class challenge_9{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number a =" + " ");
+        float a = input.nextFloat();
+        System.out.print("Enter a number b =" +" ");
+        float b = input.nextFloat();
+        
+   
+      
+        System.out.println("a X b =" +  " "+ (a*b));
+       
+    }
+}
