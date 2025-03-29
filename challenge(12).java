d9abb91df591bf9ad7fd4d34470221d043990851
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 13:33:39 +0530

    Create challenge(12).java
    Create a program to calculate simple interest

---
+++ challenge(12).java
@@ -0,0 +1,26 @@
+
+// Challenge
+// Create a program to calculate simple interest
+
+
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class challenge_10{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Principle =" + " ");
+        double a = input.nextDouble();
+        System.out.print("Rate =" +" ");
+        double b = input.nextDouble();
+         System.out.print("Time =" +" ");
+        double c = input.nextDouble();
+        
+        
+System.out.println("The Simple Interst   is " +  " "+ (a*b*c)/100);
+       
+    }
+}
+
+
