0f83f352218383111da607d8c700a6b7ec734475
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-03-29 14:09:32 +0530

    Create challenge(14).java
    Create a program to convert degree celcius into fahrenheit f = (c* 9/5 +32)

---
+++ challenge(14).java
@@ -0,0 +1,22 @@
+
+// Challenge
+// Create a program to convert degree celcius into fahrenheit f = (c* 9/5 +32)
+
+
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class challenge_14{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("fahrenhiet =" + " ");
+        double a = input.nextDouble();
+        
+        
+System.out.println("Fahrenheit = " + " "+ (a*1.8)+32);
+       
+    }
+}
+
+
