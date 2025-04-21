d89a78893f1b8ad13aac9c0bd56465fc5bb1ac87
Lalit Tiwari <ltsmartofficial@gmail.com>
2025-04-21 22:40:35 +0530

    Create areaoftriagle.java

---
+++ areaoftriagle.java
@@ -0,0 +1,16 @@
+
+import java.lang.*;
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[]  args){
+
+Scanner kallu = new Scanner(System.in);
+System.out.print("Enter base here ");
+Double base = kallu.nextDouble();
+System.out.print("Enter Height here ");
+Double height = kallu.nextDouble();
+System.out.print("The area of triagle is " + (0.5)*(base*height));    
+
+    }
+}
